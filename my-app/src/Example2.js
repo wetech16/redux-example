d4c3a4e0768801cec4eb566acde58284c8cc9d3b
Stephen Guo <wetech16@gmail.com>
2020-09-04 00:28:31 -0700

    example2

---
+++ my-app/src/Example2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {createStore } from 'redux'
+
+export default function Example2() {
+    const reducer = (state, action) => {
+        
+    }
+    return (
+        <div>
+            Example2 See console
+        </div>
+    )
+}
