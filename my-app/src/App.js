a29a3e486b2e34aba71682c8fafeaf6da83831e5
Stephen Guo <wetech16@gmail.com>
2020-09-04 00:38:27 -0700

    -simple

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Example1 from './Example1';
+// import Example1 from './Example1';
+import Example2 from './Example2';
 
 function App() {
   return (
     <div className="App">
-      <Example1 />
+      {/* <Example1 /> */}
+      <Example2 />
     </div>
   );
 }
--- my-app/src/Example2.js
+++ my-app/src/Example2.js
@@ -3,8 +3,18 @@ import {createStore } from 'redux'
 
 export default function Example2() {
     const reducer = (state, action) => {
-        
+        if(action.type === "Peace"){
+            return action.payload
+        }
+        return state;
     }
+    const store = createStore(reducer, "Peace");
+    store.subscribe(() => {
+        console.log("Ex2: Store is Now", store.getState());
+    })
+    store.dispatch({type: "ATTACK", payload: "Not PeaPeace"})
+
+
     return (
         <div>
             Example2 See console
